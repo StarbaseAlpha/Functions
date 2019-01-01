2ddc8274a5607b04964bdeb47958220f85f94f9a
Mike Fuller <emailthemike@gmail.com>
2018-12-31 21:29:28 -0500

    added package files

---
+++ functions.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const {VM} = require('vm2');
+
+function Functions() {
+
+  let functions = {};
+
+  functions.runCode = (code = null, env = {}, options = {}) => {
+    return new Promise((resolve, reject) => {
+
+      const vm = new VM({
+        "sandbox": options.sandbox || {},
+        "timeout": options.timeout || 1000
+      });
+
+      vm.freeze(resolve, 'resolve');
+      vm.freeze(reject, 'reject');
+
+      if (env && typeof env === 'object') {
+        Object.keys(env).forEach(key => {
+          vm.freeze(env[key], key);
+        });
+      }
+
+      vm.run(`
+        'use strict';
+        let func = async () => {return (` + code + `)()};
+        func().then(resolve).catch(reject);
+      `);
+    
+    });
+  };
+
+  functions.express = (code = null, env = {}, options = {}) => {
+    return (req, res) => {
+
+      if (!options || typeof options !== 'object') {
+        options = {};
+      }
+
+      if (!options.sandbox || typeof options.sandbox !== 'object') {
+        options.sandbox = {};
+      }
+
+      options.sandbox.req = {
+        "headers": req.headers || {},
+        "path": req.path || '/',
+        "url": req.url || '/',
+        "method":req.method,
+        "query": req.query || {},
+        "body": req.body || {},
+        "params": req.params || {}
+      };
+     
+      functions.runCode(code, env, options).then(result => {
+        res.json(result);
+      }).catch(err => {
+        res.status(err.code||400).json({"code":err.code||400, "message":err.message||err.toString()||"ERROR!"});
+      });
+    };
+  };
+
+  return functions;
+
+}
+
+module.exports = Functions;
